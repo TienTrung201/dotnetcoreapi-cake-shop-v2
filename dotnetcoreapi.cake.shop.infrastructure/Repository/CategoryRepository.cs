e068cda224310eda621ab5ce60094e319b2f0bff
TienTrung201 <tientrung14122012@gmail.com>
2023-11-18 01:11:15 +0700

    thêm base crud và các repository

---
+++ dotnetcoreapi.cake.shop.infrastructure/Repository/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using dotnetcoreapi.cake.shop.domain;
+using dotnetcoreapi.cake.shop.infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetcoreapi.cake.shop.infrastructure
+{
+    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(CakeShopContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ dotnetcoreapi.cake.shop.infrastructure/Repository/OrderRepository.cs
@@ -0,0 +1,34 @@
+﻿using dotnetcoreapi.cake.shop.domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetcoreapi.cake.shop.infrastructure
+{
+    public class OrderRepository : BaseRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(CakeShopContext context) : base(context)
+        {
+        }
+
+        /// <summary>
+        /// Lấy một bản ghi theo ID
+        /// </summary>
+        /// <param name="orderId">ID của bản ghi</param>
+        /// <returns></returns>
+        public override async Task<Order> GetEntityByIdAsync(int orderId)
+        {
+            var order = await _context.Orders
+                                .Include(o => o.ShippingMethod)
+                                .Include(o => o.Items)
+                                    .ThenInclude(item => item.Product)
+                                    .ThenInclude(product => product.Images)
+                                .FirstOrDefaultAsync(o => o.OrderId == orderId);
+
+            if (order == null)
+            {
+                throw new NotFoundException("Đơn hàng không tồn tại", ErrorCode.NotFound);
+            }
+
+            return order;
+        }
+    }
+}
